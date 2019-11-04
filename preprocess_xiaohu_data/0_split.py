dd476a1c30cb5d42af80b4ff2629e82f4e12b6f3
lizuoyue <li.zuoyue@foxmail.com>
2019-11-04 20:23:11 +0100

    a

--- preprocess_xiaohu_data/0_split.py
+++ preprocess_xiaohu_data/0_split.py
@@ -7,7 +7,7 @@ files = glob.glob(dataset_dir + '/*_sate_label.png')
 names = [os.path.basename(item).replace('_sate_label.png', '') for item in files]
 random.seed(7)
 random.shuffle(names)
-with open('test.txt', 'w') as f:
+with open('val.txt', 'w') as f:
 	for name in names[:64]:
 		f.write('%s\n' % name)
 with open('train.txt', 'w') as f:
--- preprocess_xiaohu_data/3_make_bad_with_depth.py
+++ preprocess_xiaohu_data/3_make_bad_with_depth.py
@@ -13,23 +13,25 @@ two_dim = np.array([
 	[255,   0],
 ])
 
-target = '../datasets/L2R_bad_with_depth'
+target = '../datasets/L2R_bad_with_depth/'
 os.makedirs(target, exist_ok = True)
-for mode in ['/train', '/val']:
+for mode in ['train', 'val']:
+	with open(mode + '.txt', 'r') as f:
+		lines = [line.strip() for line in f.readlines()]
 	os.makedirs(target + mode, exist_ok = True)
-	source_mode = mode.replace('/', '').replace('val', 'test')
-	sem_label_path = '/home/zoli/xiaohu_new_data/predict_of_%s/*_pred_sem_label.png' % source_mode
-	ss = 'predict_of_%s' % source_mode
-	tt = source_mode + '2'
-	files = glob.glob(sem_label_path)
-	for file in tqdm.tqdm(files):
-		sem = np.array(Image.open(file).resize((512, 256), PIL.Image.BILINEAR))
-		dep = np.array(Image.open(file.replace(ss, tt).replace('_pred_sem_label', '_proj_dis')).convert('L').resize((512, 256), PIL.Image.BILINEAR))
-		rgb = np.array(Image.open(file.replace(ss, tt).replace('_pred_sem_label', '_street_rgb')).resize((512, 256), PIL.Image.BILINEAR))
+
+	sem_path = '/home/zoli/xiaohu_new_data/predict_of_train/%s_pred_sem_label.png'
+	rgb_path = '/home/zoli/xiaohu_new_data/train2/%s_street_rgb.png'
+	dep_path = '/home/zoli/xiaohu_new_data/train2/%s_proj_dis.png'
+
+	for line in tqdm.tqdm(lines):
+		sem = np.array(Image.open(sem_path % line).resize((512, 256), PIL.Image.BILINEAR))
+		dep = np.array(Image.open(dep_path % line).convert('L').resize((512, 256), PIL.Image.BILINEAR))
+		rgb = np.array(Image.open(rgb_path % line).resize((512, 256), PIL.Image.BILINEAR))
 		info = rgb.copy()
 		info[..., 2] = dep
 		for i in range(5):
 			info[sem == i, :2] = two_dim[i]
 		bi = np.concatenate([info, rgb], 1)
-		basename = '/' + os.path.basename(file)
-		Image.fromarray(bi).save(target + mode + basename.replace('_pred_sem_label', ''))
+		basename = '/%s.png' % line 
+		Image.fromarray(bi).save(target + mode + basename)
