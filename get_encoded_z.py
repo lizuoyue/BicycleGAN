4b1b4a8d7c6f0099750ce7a054a336ff01bdc42e
lizuoyue <li.zuoyue@foxmail.com>
2019-11-06 10:34:23 +0100

    a

--- get_encoded_z.py
+++ get_encoded_z.py
@@ -20,18 +20,9 @@ print('Loading model %s' % testOpt.model)
 
 # test stage
 for i, data in enumerate(dataset):
-    print(data['A_paths'])
+    key = os.path.basename(data['A_paths'][0]).replace('.png', '')
     model.set_input(data)
+    print(key)
     print('process input image %3.3d/%3.3d' % (i, len(dataset)))
-    continue
-    if not opt.sync:
-        z_samples = model.get_z_random(opt.n_samples + 1, opt.nz)
-    for nn in range(opt.n_samples + 1):
-        encode = nn == 0 and not opt.no_encode
-        real_A, fake_B, real_B = model.test(z_samples[[nn]], encode=encode)
-        if nn == 0:
-            images = [real_A, real_B, fake_B]
-            names = ['input', 'ground truth', 'encoded']
-        else:
-            images.append(fake_B)
-            names.append('random_sample%2.2d' % nn)
+    z, _ = model.netE(model.real_B[..., :511])
+    print(z)
