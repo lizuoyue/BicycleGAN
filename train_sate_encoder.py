31360e84af1bece4313ca5c60abf001572a9600d
lizuoyue <li.zuoyue@foxmail.com>
2019-11-06 11:05:14 +0100

    a

--- train_sate_encoder.py
+++ train_sate_encoder.py
@@ -11,13 +11,21 @@ if __name__=='__main__':
 	criterionL1 = torch.nn.L1Loss()
 	optimizer_E = torch.optim.Adam(netE.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
 
-	for item in []:
-		rgb = None
-		sate = None
-		optimizer.zero_grad()
-		z_target = netE(rgb)
-		z_pred = netE(sate)
+	for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
 
-		loss = criterionL1(z_pred, z_target)
-		loss.backward()
-		optimizer_E.step()
+		epoch_start_time = time.time()  # timer for entire epoch
+        iter_data_time = time.time()    # timer for data loading per iteration
+        epoch_iter = 0                  # the number of training iterations in current epoch, reset to 0 every epoch
+
+        for i, data in enumerate(dataset):  # inner loop within one epoch
+            iter_start_time = time.time()  # timer for computation per iteration
+            
+			rgb = None
+			sate = None
+			optimizer.zero_grad()
+			z_target = netE(rgb)
+			z_pred = netE(sate)
+
+			loss = criterionL1(z_pred, z_target)
+			loss.backward()
+			optimizer_E.step()
