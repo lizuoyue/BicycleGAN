baf8e7cb7b8b192fcfcd2da7404b52a21d5443af
lizuoyue <li.zuoyue@foxmail.com>
2019-11-06 10:11:42 +0100

    a

---
+++ test_get_encoded_z.py
@@ -0,0 +1,39 @@
+import os
+from options.test_options import TestOptions
+from data import create_dataset
+from models import create_model
+from util.visualizer import save_images
+from itertools import islice
+from util import html
+
+
+# options
+opt = TestOptions().parse()
+opt.num_threads = 1   # test code only supports num_threads=1
+opt.batch_size = 1   # test code only supports batch_size=1
+opt.serial_batches = True  # no shuffle
+
+# create dataset
+dataset = create_dataset(opt)
+model = create_model(opt)
+model.setup(opt)
+model.eval()
+print('Loading model %s' % opt.model)
+
+# test stage
+for i, data in enumerate(islice(dataset, opt.num_test)):
+    print(data)
+    model.set_input(data)
+    print('process input image %3.3d/%3.3d' % (i, opt.num_test))
+    continue
+    if not opt.sync:
+        z_samples = model.get_z_random(opt.n_samples + 1, opt.nz)
+    for nn in range(opt.n_samples + 1):
+        encode = nn == 0 and not opt.no_encode
+        real_A, fake_B, real_B = model.test(z_samples[[nn]], encode=encode)
+        if nn == 0:
+            images = [real_A, real_B, fake_B]
+            names = ['input', 'ground truth', 'encoded']
+        else:
+            images.append(fake_B)
+            names.append('random_sample%2.2d' % nn)
