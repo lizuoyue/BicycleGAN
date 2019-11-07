d842082d1adc71e94cf201c555693d69e8141093
lizuoyue <li.zuoyue@foxmail.com>
2019-11-07 22:16:54 +0100

    a

--- eval.py
+++ eval.py
@@ -10,13 +10,20 @@ ssim = tf.image.ssim(im1, im2, max_val=255)
 sess = tf.Session()
 
 dataset = 'L2R_good_with_depth'
-gts, sas = [], []
+gts, ens, sas = [], [], []
 for file in glob.glob('results/%s/val_sync/images/*_ground_truth.png' % dataset):
 	gt = str(file)
 	en = gt.replace('_ground_truth', '_encoded')
 	sa = gt.replace('_ground_truth', '_encoded_satellite')
 	# Read images from file.
 	gts.append(np.array(Image.open(gt)))
+	ens.append(np.array(Image.open(en)))
 	sas.append(np.array(Image.open(sa)))
 
-print(sess.run([psnr, ssim], feed_dict={im1: gts, im2: sas}))
+psnr_val, ssim_val = sess.run([psnr, ssim], feed_dict={im1: gts, im2: ens})
+print('PSNR: ', psnr_val.mean())
+print('SSIM: ', ssim_val.mean())
+print()
+psnr_val, ssim_val = sess.run([psnr, ssim], feed_dict={im1: gts, im2: sas})
+print('PSNR: ', psnr_val.mean())
+print('SSIM: ', ssim_val.mean())
