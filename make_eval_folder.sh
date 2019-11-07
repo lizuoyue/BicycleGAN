ffe5d6306b4594911ecfe7c3056114519c02ac6d
lizuoyue <li.zuoyue@foxmail.com>
2019-11-07 21:10:57 +0100

    a

--- make_eval_folder.sh
+++ make_eval_folder.sh
@@ -1,22 +1,25 @@
-DATASET='L2R_good_with_depth'
-GT='./eval/gt'
-EN='./eval/encoded'
-SA='./eval/encoded_sate'
+DATASET="L2R_good_with_depth"
+GT="./eval/gt"
+EN="./eval/encoded"
+SA="./eval/encoded_sate"
 mkdir -p ${GT}
 mkdir -p ${EN}
 mkdir -p ${SA}
-cp './results/${DATASET}/val_sync/images/*_ground truth.png' ${GT}
-cp './results/${DATASET}/val_sync/images/*_encoded.png' ${EN}
-cp './results/${DATASET}/val_sync/images/*_encoded_satellite.png' ${SA}
-for FILE in $(ls ${GT})
+cp  ${GT}
+cp "./results/${DATASET}/val_sync/images/*_encoded.png" ${EN}
+cp "./results/${DATASET}/val_sync/images/*_encoded_satellite.png" ${SA}
+for FILE in $(ls ./results/${DATASET}/val_sync/images/*_ground\ truth.png)
 do
-	mv ${FILE} ${FILE//_groundtruth/}
+	BN="$(basename -- $FILE)"
+	mv ${FILE} ${GT}"/"${BN//_groundtruth/}
 done
 for FILE in $(ls ${EN})
 do
-	mv ${FILE} ${FILE//_encoded/}
+	BN="$(basename -- $FILE)"
+	mv ${FILE} ${EN}"/"${BN//_encoded/}
 done
 for FILE in $(ls ${SA})
 do
-	mv ${FILE} ${FILE//_encoded_satellite/}
+	BN="$(basename -- $FILE)"
+	mv ${FILE} ${SA}"/"${BN//_encoded_satellite/}
 done
