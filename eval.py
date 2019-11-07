afd085513903d6b6d9f8586e52025fb9d007f38a
lizuoyue <li.zuoyue@foxmail.com>
2019-11-07 21:55:39 +0100

    a

--- eval.py
+++ eval.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import glob
 
-sess = tf.Session()
 dataset = 'L2R_good_with_depth'
 for file in glob.glob('./results/%s/val_sync/images/*_ground_truth.png' % dataset):
 	gt = file
@@ -13,4 +12,6 @@ for file in glob.glob('./results/%s/val_sync/images/*_ground_truth.png' % datase
 	# Compute SSIM over tf.uint8 Tensors.
 	psnr = tf.image.psnr(im1, im2, max_val=255)
 	ssim = tf.image.ssim(im1, im2, max_val=255)
+
+	sess = tf.Session()
 	print(sess.run(psnr), sess.run(ssim))
