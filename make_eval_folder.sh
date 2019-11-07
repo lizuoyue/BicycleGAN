036909efa8999ee1afefb3332a7e4eb81915b0b2
lizuoyue <li.zuoyue@foxmail.com>
2019-11-07 21:03:57 +0100

    a

---
+++ make_eval_folder.sh
@@ -0,0 +1,22 @@
+DATASET='L2R_good_with_depth'
+GT='./eval/gt'
+EN='./eval/encoded'
+SA='./eval/encoded_sate'
+mkdir -p ${GT}
+mkdir -p ${EN}
+mkdir -p ${SA}
+cp ./results/${DATASET}/val_sync/images/*_groundtruth.png ${GT}
+cp ./results/${DATASET}/val_sync/images/*_encoded.png ${EN}
+cp ./results/${DATASET}/val_sync/images/*_encoded_satellite.png ${SA}
+for FILE in $(ls ${GT})
+do
+	mv FILE ${FILE//_groundtruth/}
+done
+for FILE in $(ls ${EN})
+do
+	mv FILE ${FILE//_encoded/}
+done
+for FILE in $(ls ${SA})
+do
+	mv FILE ${FILE//_encoded_satellite/}
+done
