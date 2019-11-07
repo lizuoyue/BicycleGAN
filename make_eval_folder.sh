815f795af159d56e3c38886a20f3d02d402dcb76
lizuoyue <li.zuoyue@foxmail.com>
2019-11-07 21:06:22 +0100

    a

--- make_eval_folder.sh
+++ make_eval_folder.sh
@@ -5,18 +5,18 @@ SA='./eval/encoded_sate'
 mkdir -p ${GT}
 mkdir -p ${EN}
 mkdir -p ${SA}
-cp ./results/${DATASET}/val_sync/images/*_groundtruth.png ${GT}
-cp ./results/${DATASET}/val_sync/images/*_encoded.png ${EN}
-cp ./results/${DATASET}/val_sync/images/*_encoded_satellite.png ${SA}
+cp './results/${DATASET}/val_sync/images/*_ground truth.png' ${GT}
+cp './results/${DATASET}/val_sync/images/*_encoded.png' ${EN}
+cp './results/${DATASET}/val_sync/images/*_encoded_satellite.png' ${SA}
 for FILE in $(ls ${GT})
 do
-	mv FILE ${FILE//_groundtruth/}
+	mv ${FILE} ${FILE//_groundtruth/}
 done
 for FILE in $(ls ${EN})
 do
-	mv FILE ${FILE//_encoded/}
+	mv ${FILE} ${FILE//_encoded/}
 done
 for FILE in $(ls ${SA})
 do
-	mv FILE ${FILE//_encoded_satellite/}
+	mv ${FILE} ${FILE//_encoded_satellite/}
 done
