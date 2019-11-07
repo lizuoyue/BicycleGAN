f68657efc3457788bff07467a6f48996af8c98de
lizuoyue <li.zuoyue@foxmail.com>
2019-11-07 22:13:16 +0100

    a

--- eval.py
+++ eval.py
@@ -1,5 +1,13 @@
 import tensorflow as tf
+import numpy as np
 import glob
+from PIL import Image
+
+im1 = tf.placeholder(tf.uint8, [None, None, None, None])
+im2 = tf.placeholder(tf.uint8, [None, None, None, None])
+psnr = tf.image.psnr(im1, im2, max_val=255)
+ssim = tf.image.ssim(im1, im2, max_val=255)
+sess = tf.Session()
 
 dataset = 'L2R_good_with_depth'
 for file in glob.glob('results/%s/val_sync/images/*_ground_truth.png' % dataset):
@@ -7,12 +15,7 @@ for file in glob.glob('results/%s/val_sync/images/*_ground_truth.png' % dataset)
 	en = gt.replace('_ground_truth', '_encoded')
 	sa = gt.replace('_ground_truth', '_encoded_satellite')
 	# Read images from file.
-	im1 = tf.io.decode_png(gt)
-	im2 = tf.io.decode_png(sa)
-	# Compute SSIM over tf.uint8 Tensors.
-	psnr = tf.image.psnr(im1, im2, max_val=255)
-	ssim = tf.image.ssim(im1, im2, max_val=255)
+	gts.append(np.array(Image.open(gt)))
+	sas.append(np.array(Image.open(sa)))
 
-	print(psnr, ssim)
-	sess = tf.Session()
-	print(sess.run(psnr), sess.run(ssim))
+print(sess.run([psnr, ssim], feed_dict={im1: gts, im2: sas}))
