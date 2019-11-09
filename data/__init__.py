c9cce814faa1b35078aad26caf9fca8c0bd26015
lizuoyue <li.zuoyue@foxmail.com>
2019-11-09 14:03:37 +0100

    a

--- data/__init__.py
+++ data/__init__.py
@@ -72,6 +72,8 @@ class CustomDatasetDataLoader():
         dataset_class = find_dataset_using_name(opt.dataset_mode)
         self.dataset = dataset_class(opt)
         print("dataset [%s] was created" % type(self.dataset).__name__)
+        print(opt.batch_size)
+        input()
         self.dataloader = torch.utils.data.DataLoader(
             self.dataset,
             batch_size=opt.batch_size,
