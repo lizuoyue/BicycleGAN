24829b9fa106b4f9346491704522be7eb518e1b3
lizuoyue <li.zuoyue@foxmail.com>
2019-11-11 11:44:41 +0100

    a

--- preprocess_augmented_data/5_make_good_without_depth_test.py
+++ preprocess_augmented_data/5_make_good_without_depth_test.py
@@ -23,16 +23,15 @@ dep_files = sorted(glob.glob(dep_path))
 assert(len(sem_files) == len(rgb_files))
 assert(len(sem_files) == len(dep_files))
 
-target = '../datasets/L2R_aug_good_with_depth/'
+target = '../datasets/L2R_aug_good_without_depth/'
 os.makedirs(target, exist_ok = True)
 for mode in ['val']:
 	os.makedirs(target + mode, exist_ok = True)
 	for sem_file, rgb_file, dep_file in tqdm.tqdm(zip(sem_files, rgb_files, dep_files), total=len(rgb_files)):
 		sem = np.array(Image.open(sem_file).resize((512, 256), PIL.Image.BILINEAR))
-		dep = np.array(Image.open(dep_file).convert('L').resize((512, 256), PIL.Image.BILINEAR))
 		rgb = np.array(Image.open(rgb_file).resize((512, 256), PIL.Image.BILINEAR))
 		info = rgb.copy()
-		info[..., 2] = dep
+		info[..., 2] = 0
 		for i in range(5):
 			info[sem == i, :2] = two_dim[i]
 		bi = np.concatenate([info, rgb], 1)
