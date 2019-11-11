69d17c52bbda02685ec6e6ba0a92fc2caba6eede
lizuoyue <li.zuoyue@foxmail.com>
2019-11-11 12:18:48 +0100

    a

--- models/bicycle_gan_model.py
+++ models/bicycle_gan_model.py
@@ -171,13 +171,9 @@ class BiCycleGANModel(BaseModel):
         self.loss_G_P = 0.0
         if self.opt.lambda_P > 0.0:
             for i in range(self.fake_B_encoded.size(0)):
-                print(self.fake_B_encoded[i:i+1].shape)
-                print(torch.min(self.fake_B_encoded[i:i+1]))
-                print(torch.max(self.fake_B_encoded[i:i+1]))
-                print(self.real_B_encoded[i:i+1].shape)
-                print(torch.min(self.real_B_encoded[i:i+1]))
-                print(torch.max(self.real_B_encoded[i:i+1]))
-                tmp = self.criterionPerceptual.forward(self.fake_B_encoded[i:i+1], self.real_B_encoded[i:i+1], normalize=True)
+                pred = (self.fake_B_encoded[i:i+1] + 1.0) / 2.0
+                ref = (self.real_B_encoded[i:i+1] + 1.0) / 2.0
+                tmp = self.criterionPerceptual.forward(pred, ref, normalize=True)
                 print(tmp)
                 self.loss_G_P += tmp
             self.loss_G_P *= (self.opt.lambda_P / self.fake_B_encoded.size(0))
