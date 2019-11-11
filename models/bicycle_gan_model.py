631653a849a4e9bc2bb045921b888cb1159faf77
lizuoyue <li.zuoyue@foxmail.com>
2019-11-11 01:33:24 +0100

    a

--- models/bicycle_gan_model.py
+++ models/bicycle_gan_model.py
@@ -173,7 +173,6 @@ class BiCycleGANModel(BaseModel):
             for i in range(self.fake_B_encoded.size(0)):
                 tmp = self.criterionPerceptual.forward(self.fake_B_encoded[i:i+1], self.real_B_encoded[i:i+1], normalize=True) 
                 self.loss_G_P += tmp
-                print(tmp)
             self.loss_G_P *= (self.opt.lambda_P / self.fake_B_encoded.size(0))
 
         self.loss_G = self.loss_G_GAN + self.loss_G_GAN2 + self.loss_G_L1 + self.loss_kl + self.loss_G_P
