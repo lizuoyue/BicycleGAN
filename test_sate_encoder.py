27a2bd0ebb69bf4ac3cd6c27944972607372ae9f
lizuoyue <li.zuoyue@foxmail.com>
2019-11-13 14:36:50 +0100

    a

--- test_sate_encoder.py
+++ test_sate_encoder.py
@@ -31,7 +31,7 @@ print('Loading model %s' % opt.model)
 sateOpt = SateOption()
 sateE = networks.define_E(sateOpt.output_nc, sateOpt.nz, sateOpt.nef, netE=sateOpt.netE, norm=sateOpt.norm, nl=sateOpt.nl,
                             init_type=sateOpt.init_type, init_gain=sateOpt.init_gain, gpu_ids=sateOpt.gpu_ids, vaeLike=sateOpt.use_vae)
-sateCheckpoint = torch.load('sate_encoder/sate_encoder_latest.pth')
+sateCheckpoint = torch.load('sate_encoder/sate_encoder_aug_expand_latest.pth')
 sateE.load_state_dict(sateCheckpoint['model_state_dict'])
 sateE.eval()
 transforms = torchvision.transforms.Compose([
@@ -52,7 +52,7 @@ if opt.sync:
 for i, data in enumerate(islice(dataset, opt.num_test)):
     model.set_input(data)
     key = os.path.basename(data['A_paths'][0]).replace('_street_rgb_00.png', '')
-    # sate_path = '/home/zoli/xiaohu_new_data/test_augment/test_0/%s_sate_rgb_00.png' % key
+    sate_path = '/home/zoli/xiaohu_new_data/test_augment/test_0/%s_sate_rgb_00.png' % key
     print('process input image %3.3d/%3.3d, %s' % (i, opt.num_test, key))
     if not opt.sync:
         z_samples = model.get_z_random(opt.n_samples + 1, opt.nz)
@@ -63,11 +63,11 @@ for i, data in enumerate(islice(dataset, opt.num_test)):
             images = [real_A, real_B, fake_B]
             names = ['input', 'ground_truth', 'encoded']
             ###
-            # with torch.no_grad():
-            #     sate_rgb = transforms(Image.open(sate_path)).to(sateOpt.device)
-            #     z0, _ = sateE(sate_rgb.unsqueeze(0))
-            #     images.append(model.netG(model.real_A, z0))
-            #     names.append('encoded_satellite')
+            with torch.no_grad():
+                sate_rgb = transforms(Image.open(sate_path)).to(sateOpt.device)
+                z0, _ = sateE(sate_rgb.unsqueeze(0))
+                images.append(model.netG(model.real_A, z0))
+                names.append('encoded_satellite')
             ###
         else:
             images.append(fake_B)
