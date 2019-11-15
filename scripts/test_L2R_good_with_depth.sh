1c2d2d2926e3cf724f99d228d55220be6d88a338
lizuoyue <li.zuoyue@foxmail.com>
2019-11-15 16:44:24 +0100

    a

--- scripts/test_L2R_good_with_depth.sh
+++ scripts/test_L2R_good_with_depth.sh
@@ -30,7 +30,7 @@ cp ${E_PATH} ${LATEST_DIR}
 # misc
 GPU_ID=0   # gpu id
 NUM_TEST=100 # number of input images duirng test
-NUM_SAMPLES=5 # number of samples per input images
+NUM_SAMPLES=10 # number of samples per input images
 
 # command
 CUDA_VISIBLE_DEVICES=${GPU_ID} python3 ./test_sate_encoder.py \
